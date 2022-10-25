7c443c8ef265b6574d24c96028f98a222d7e58f6
DarkThunder99 <75782012+DarkThunder99@users.noreply.github.com>
2022-10-25 10:08:38 +0530

    Add files via upload

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+} from "react-icons/di";
+import {
+  SiFirebase,
+} from "react-icons/si";
+import {
+  AiOutlineHtml5,
+} from "react-icons/ai";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <CgCPlusPlus />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <AiOutlineHtml5 />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
